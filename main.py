170a0626625080fe00816905f7f0fa8486288cf3
Pramit Mallick <pramitmallick@gmail.com>
2018-10-24 00:43:27 -0400

    pickle dump of epoch loss and acc

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.autograd import Variable
+import pickle
+import collections
 
 # Training settings
 parser = argparse.ArgumentParser(description='PyTorch GTSRB example')
@@ -13,8 +15,8 @@ parser.add_argument('--data', type=str, default='data', metavar='D',
                     help="folder where data is located. train_data.zip and test_data.zip need to be found in the folder")
 parser.add_argument('--batch-size', type=int, default=64, metavar='N',
                     help='input batch size for training (default: 64)')
-parser.add_argument('--epochs', type=int, default=10, metavar='N',
-                    help='number of epochs to train (default: 10)')
+parser.add_argument('--epochs', type=int, default=100, metavar='N',
+                    help='number of epochs to train (default: 1)')
 parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
                     help='learning rate (default: 0.01)')
 parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
@@ -47,13 +49,21 @@ model = Net()
 
 optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
 
-def train(epoch):
+def train(epoch, convergencePlots):
     model.train()
+    sumLoss = 0
+    numBatches = 0
+    correct = 0
+    train_loss = 0
     for batch_idx, (data, target) in enumerate(train_loader):
+        numBatches += 1
         data, target = Variable(data), Variable(target)
         optimizer.zero_grad()
         # print("data", data.size())
         output = model(data)
+        train_loss += F.cross_entropy(output, target, size_average=False).data[0] # sum up batch loss
+        pred = output.data.max(1, keepdim=True)[1] # get the index of the max log-probability
+        correct += pred.eq(target.data.view_as(pred)).cpu().sum()
         # print("output", output.size())
         # print("target", target.size())
         # break
@@ -65,15 +75,20 @@ def train(epoch):
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss.data[0]))
+        sumLoss += loss.data[0]
 
-def validation():
+    train_loss /= len(train_loader.dataset)
+    convergencePlots['training_avg_loss'].append(train_loss)
+    convergencePlots['training_avg_acc'].append(100. * correct / len(train_loader.dataset))
+
+def validation(convergencePlots):
     model.eval()
     validation_loss = 0
     correct = 0
     for data, target in val_loader:
         data, target = Variable(data, volatile=True), Variable(target)
         output = model(data)
-        validation_loss += F.nll_loss(output, target, size_average=False).data[0] # sum up batch loss
+        validation_loss += F.cross_entropy(output, target, size_average=False).data[0] # sum up batch loss
         pred = output.data.max(1, keepdim=True)[1] # get the index of the max log-probability
         correct += pred.eq(target.data.view_as(pred)).cpu().sum()
 
@@ -81,12 +96,15 @@ def validation():
     print('\nValidation set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
         validation_loss, correct, len(val_loader.dataset),
         100. * correct / len(val_loader.dataset)))
+    convergencePlots['validation_avg_loss'].append(validation_loss)
+    convergencePlots['validation_avg_acc'].append(100. * correct / len(val_loader.dataset))
 
-
+convergencePlots = collections.defaultdict(list)
 for epoch in range(1, args.epochs + 1):
-    train(epoch)
-    validation()
+    train(epoch, convergencePlots)
+    validation(convergencePlots)
     # model_file = 'model_' + str(epoch) + '.pth'
     model_file = 'model_latest.pth'
     torch.save(model.state_dict(), model_file)
     print('\nSaved model to ' + model_file + '. You can run `python evaluate.py ' + model_file + '` to generate the Kaggle formatted csv file')
+    pickle.dump( convergencePlots, open( "convergencePlots.p", "wb" ) )
